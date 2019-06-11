b98be6e9216ea7c25f833eef6bc9ea3551305077
Lukáš Opekar <lukas@sinfin.cz>
2019-06-11 15:16:51 +0200

    update

--- main.py
+++ main.py
@@ -13,6 +13,7 @@ flags.DEFINE_float("learning_rate", 0.0002, "Learning rate of for adam [0.0002]"
 flags.DEFINE_float("beta1", 0.5, "Momentum term of adam [0.5]")
 flags.DEFINE_float("train_size", np.inf, "The size of train images [np.inf]")
 flags.DEFINE_integer("batch_size", 64, "The size of batch images [64]")
+flags.DEFINE_integer("c_dim", 3, "Dimension of image color. For grayscale input, set to 1. ")
 flags.DEFINE_integer("input_height", 108, "The size of image to use (will be center cropped). [108]")
 flags.DEFINE_integer("input_width", None, "The size of image to use (will be center cropped). If None, same value as input_height [None]")
 flags.DEFINE_integer("output_height", 64, "The size of the output images to produce [64]")
@@ -40,14 +41,14 @@ def main(_):
     os.makedirs(FLAGS.checkpoint_dir)
   if not os.path.exists(FLAGS.sample_dir):
     os.makedirs(FLAGS.sample_dir)
-  
+
   gpu_options = tf.GPUOptions(per_process_gpu_memory_fraction=0.333)
   run_config = tf.ConfigProto(gpu_options=gpu_options)
   #run_confing = tf.Session(config=tf.ConfigProto(gpu_options=gpu_options))
   run_config.gpu_options.allow_growth=True
-  
- 
-  
+
+
+
 
 
   with tf.Session(config=run_config) as sess:
@@ -92,7 +93,7 @@ def main(_):
     else:
       if not dcgan.load(FLAGS.checkpoint_dir)[0]:
         raise Exception("[!] Train a model first, then run test mode")
-      
+
 
     # to_json("./web/js/layers.js", [dcgan.h0_w, dcgan.h0_b, dcgan.g_bn0],
     #                 [dcgan.h1_w, dcgan.h1_b, dcgan.g_bn1],
